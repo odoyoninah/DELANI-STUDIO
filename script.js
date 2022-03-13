88d960254d87a4e35282ad77c6cebf7f7589b151
odoyonina <odoyoninah@gmail.com>
2022-03-14 00:25:53 +0300

    style porfolio

--- script.js
+++ script.js
@@ -27,3 +27,23 @@ $(".services3").click(function(){
     $(".services3").hide()
     $(".product").show()
 })
+
+// $("folio").click(function(){
+//     $("folio1").alert("DELANI STUDIO");
+//     $("folio1").fadeIn("DELANI STUDIO");
+//     $("folio1").fadeIn("");
+//   });
+
+var folio = [0,1,2,3,4,5,6,7]
+folio.forEach(function(ele,index){
+    index=index+1
+    $(".folio"+index).mouseover(function(){
+        $(".folio"+index+" .folio").show()
+        $(".folio"+index).addClass("darken")
+    })
+    $(".folio"+index).mouseout(function(){
+        $(".folio"+index+" .folio").hide()
+        $(".folio"+index).removeClass("darken")
+    })  
+    
+})
