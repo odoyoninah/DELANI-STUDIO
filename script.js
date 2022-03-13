c88a46a477aea4b770fa56b5abe9f8b6ff55196b
odoyonina <odoyoninah@gmail.com>
2022-03-13 15:43:37 +0300

    use jquery to hide and show icons

---
+++ script.js
@@ -0,0 +1,29 @@
+$(".design").click(function(){
+    $(".design").hide()
+    $(".services1").show()
+})
+
+$(".services1").click(function(){
+    $(".services1").hide()
+    $(".design").show()
+})
+
+$(".development").click(function(){
+    $(".development").hide()
+    $(".services2").show()
+})
+
+$(".services2").click(function(){
+    $(".services2").hide()
+    $(".development").show()
+})
+
+$(".product").click(function(){
+    $(".product").hide()
+    $(".services3").show()
+})
+
+$(".services3").click(function(){
+    $(".services3").hide()
+    $(".product").show()
+})
